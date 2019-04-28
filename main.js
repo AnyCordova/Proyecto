c9c06dfd739ea3c50fedb2ed13b2e19f98be30ff
AnyCordova <48812925+AnyCordova@users.noreply.github.com>
2019-04-28 14:25:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAU8-qyIzMRgeLrbkNe7vSCDp1qrXIQUEs",
+    authDomain: "proyecto-96973.firebaseapp.com",
+    databaseURL: "https://proyecto-96973.firebaseio.com",
+    projectId: "proyecto-96973",
+    storageBucket: "proyecto-96973.appspot.com",
+    messagingSenderId: "267788988413"
+  };
+  firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
